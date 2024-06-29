d34f05eab67c975111b7462fb62a603f77a69b3f
JosuaLimbu <s2200409@student.unklab.ac.id>
2024-06-29 23:28:17 +0800

    array function

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,62 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('page/home/home', ['title' => 'Home']);
+    return view('home', ['title' => 'Home']);
 });
 
 Route::get('/about', function () {
-    return view('page/about/about', ['name' => 'Josua Limbu', 'title' => 'About']);
+    return view('about', ['name' => 'Josua Limbu', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('page/blog/blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'title' => 'Project 1',
+            'author' => 'Josua Limbu',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea amet dolorum aspernatur,
+            velit corporis cumque
+            deserunt nobis sit porro provident quidem hic reiciendis eaque ipsa eum est minima dolorem a?'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Project 2',
+            'author' => 'Josua Limbu',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim excepturi reiciendis
+            ratione suscipit harum laborum autem, eligendi, dolorem dignissimos rerum odit fugit quibusdam, dolores eum
+            inventore quo iure repellat eos!'
+        ],
+    ]]);
+});
+
+Route::get('/posts/{id}', function ($id) {
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'Project 1',
+            'author' => 'Josua Limbu',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea amet dolorum aspernatur,
+            velit corporis cumque
+            deserunt nobis sit porro provident quidem hic reiciendis eaque ipsa eum est minima dolorem a?'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Project 2',
+            'author' => 'Josua Limbu',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim excepturi reiciendis
+            ratione suscipit harum laborum autem, eligendi, dolorem dignissimos rerum odit fugit quibusdam, dolores eum
+            inventore quo iure repellat eos!'
+        ],
+    ];
+
+    $post = Arr::first($posts, function($post) use ($id){
+        return $post['id'] == $id;
+    }); 
 });
 
 Route::get('/contact', function () {
-    return view('page/contact/contact', ['title' => 'Contact']);
+    return view('contact', ['title' => 'Contact']);
 });
