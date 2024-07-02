14291498855b78969fa6d792e103cb4a55f0df9d
JosuaLimbu <s2200409@student.unklab.ac.id>
2024-07-02 17:26:53 +0800

    eloquent ORM & get data from database

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,44 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-project-1',
-                'title' => 'Project 1',
-                'author' => 'Josua Limbu',
-                'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea amet dolorum aspernatur,
-                velit corporis cumque
-                deserunt nobis sit porro provident quidem hic reiciendis eaque ipsa eum est minima dolorem a?'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-project-2',
-                'title' => 'Project 2',
-                'author' => 'Josua Limbu',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim excepturi reiciendis
-                ratione suscipit harum laborum autem, eligendi, dolorem dignissimos rerum odit fugit quibusdam, dolores eum
-                inventore quo iure repellat eos!'
-            ],
-        ];
-    }
+//    protected $table = 'blog_posts';
+//    protected $primaryKey = 'post_id';
 
-    public static function find($slug): array
-    {
-        
-
-        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
-
-        if(! $post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 
-
-
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
 });
@@ -19,10 +17,7 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts'=> Post::all() ]);
 });
 
-Route::get('/posts/{slug}', function ($slug) {
-
-    $post = Post::find($slug);
-
+Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
