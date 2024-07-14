d41b68024af67725ef9af8b14929a1b31067165c
JosuaLimbu <s2200409@student.unklab.ac.id>
2024-07-14 16:00:35 +0800

    category models post

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -24,6 +25,10 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 
 Route::get('/authors/{user:username}', function (User $user) {
     return view('posts', ['title' => count($user->posts) . ' Project by ' . $user->name, 'posts' => $user->posts]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('posts', ['title' =>  ' Project in: ' . $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {
