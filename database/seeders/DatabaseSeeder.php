37207c715cffda4dd515c0471fc2d16343901553
JosuaLimbu <s2200409@student.unklab.ac.id>
2024-07-17 19:24:34 +0800

    database seeder feature in laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Post;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +19,33 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+    
+
+        // Category::create([
+        //     'name' => 'Laravel 11',
+        //     'slug' => 'laravel-11',
+        // ]); 
+
+        // Post::create([
+        //     'title' => 'Project 1',
+        //     'author_id' => 1,
+        //     'category_id' => 1,
+        //     'slug' => 'project-1',
+        //     'body' => 'loreum ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
+        // ]);
+
+        $this->call([
+            UserSeeder::class,
+            CategorySeeder::class,
         ]);
+        Post::factory(100)->recycle([
+            Category::all(),
+            User::all()
+        ])->create();
     }
 }
