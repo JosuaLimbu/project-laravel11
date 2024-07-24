51ea5e0c41ba36a8dbc2c266aaba6e8b08d9d678
JosuaLimbu <s2200409@student.unklab.ac.id>
2024-07-24 16:47:10 +0800

    solve multiple queries

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,10 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts'=> Post::all() ]);
+    // $posts = Post::with(['author','category'])->latest()->get();
+    $posts = Post::latest()->get();
+
+    return view('posts', ['title' => 'Blog', 'posts'=> $posts]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
@@ -24,10 +27,12 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 });
 
 Route::get('/authors/{user:username}', function (User $user) {
+    // $posts = $user->posts->load(['category', 'author']);
     return view('posts', ['title' => count($user->posts) . ' Project by ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
+    // $posts = $category->posts->load(['category', 'author']);
     return view('posts', ['title' =>  ' Project in: ' . $category->name, 'posts' => $category->posts]);
 });
 
