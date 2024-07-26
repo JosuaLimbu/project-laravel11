b2ea50aec0e0b2473c6ecd7680cbd4b219b7af2c
JosuaLimbu <s2200409@student.unklab.ac.id>
2024-07-26 12:22:09 +0800

    searching feature in laravel

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,8 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Josua Limbu', 'title' => 'About']);
 });
 
-Route::get('/posts', function () {
-    // $posts = Post::with(['author','category'])->latest()->get();
-    $posts = Post::latest()->get();
-
-    return view('posts', ['title' => 'Blog', 'posts'=> $posts]);
+Route::get('/posts', function () {   
+    return view('posts', ['title' => 'Blog', 'posts'=> Post::filter(request(['search', 'category', 'author']))->latest()->get()]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
@@ -27,12 +24,10 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 });
 
 Route::get('/authors/{user:username}', function (User $user) {
-    // $posts = $user->posts->load(['category', 'author']);
     return view('posts', ['title' => count($user->posts) . ' Project by ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    // $posts = $category->posts->load(['category', 'author']);
     return view('posts', ['title' =>  ' Project in: ' . $category->name, 'posts' => $category->posts]);
 });
 
